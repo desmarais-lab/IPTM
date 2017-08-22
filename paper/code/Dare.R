26e997266874443df1bd9d43286cf6f7c05b952c
bomin8319 <bomin8319>
2017-08-22 14:19:05 -0400

    GiR results

---
+++ paper/code/Dare.R
@@ -0,0 +1,22 @@
+library(IPTM)
+load('Darenew.RData')
+# 762 - 
+attach(Dare)
+Dare$node = 1:nrow(Dare$node)
+Dare$text = Dare$text[762:length(Dare$edge)]
+Dare$edge = Dare$edge[762:length(Dare$edge)]
+Dare$edge = Dare$edge[-which(sapply(Dare$text, function(d){length(d)})==0)]
+Dare$text = Dare$text[-which(sapply(Dare$text, function(d){length(d)})==0)]
+mintime = Dare$edge[[1]][[3]]
+for (n in 1:length(Dare$edge)){
+  Dare$edge[[n]][3] = (Dare$edge[[n]][[3]] - mintime) / 3600
+}
+Dare$edge = lapply(Dare$edge, function(x){x[1:3]})
+
+
+Daretest <- IPTM_inference.data(Dare$edge, Dare$node, Dare$text, Dare$vocab, nIP = 2, K = 20, sigma_Q = c(0.0005, 0.01),
+                        alpha = 2, mvec = rep(1/20, 20), betas = 2, nvec = rep(1/length(Dare$vocab), length(Dare$vocab)), 
+                        prior.b.mean = c(-3, rep(0, 24)), 
+                       prior.b.var = 1 * diag(25), prior.delta = c(0, 1), out = 100, n_B = 15000, n_d = 1500, burn = c(10000,500), 
+                       thinning = c(10,5), netstat = c("intercept", "dyadic", "degree", "triadic"), plot = FALSE, optimize = TRUE)
+save(Daretest, file = "Daretest.RData")
