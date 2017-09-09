8e2928194fc1c39a1bd0cd4fbaf313dd34879980
bomin8319 <bomin8319>
2017-09-08 20:28:54 -0400

    multiple codes for topic coherence test

---
+++ paper/code/HPC/Dare_IPTM2.R
@@ -0,0 +1,19 @@
+library(IPTM)
+load('Darenew.RData')
+attach(Dare)
+Dare$node = 1:nrow(Dare$node)
+Dare$edge = Dare$edge[-which(sapply(Dare$text, function(d){length(d)})==0)]
+Dare$text = Dare$text[-which(sapply(Dare$text, function(d){length(d)})==0)]
+mintime = Dare$edge[[1]][[3]]
+for (n in 1:length(Dare$edge)){
+  Dare$edge[[n]][3] = (Dare$edge[[n]][[3]] - mintime) / 3600
+}
+Dare$edge = lapply(Dare$edge, function(x){x[1:3]})
+
+Daretest <- IPTM_inference.data(Dare$edge, Dare$node, Dare$text, Dare$vocab, nIP = 2, K = 20, sigma_Q = c(0.005, 0.1),
+                                alpha = 2, mvec = rep(1/20, 20), betas = 2, nvec = rep(1/length(Dare$vocab), length(Dare$vocab)), 
+prior.b.mean = c(-3, rep(0, 24)),
+prior.b.var = 1 * diag(25), prior.delta = c(0, 1), out = 100, n_B = 1500, n_d = 150, burn = c(500,50),
+thinning = c(1,1), netstat = c("intercept", "dyadic"), optimize = TRUE)
+
+save(Daretest, file = "Daretest_IPTM_K20.RData")
