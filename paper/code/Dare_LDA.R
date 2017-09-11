2bb4805ccfb75a6e87caddc4c145924b3706c796
bomin8319 <bomin8319>
2017-09-11 13:19:21 -0400

    Algorithm 7 updated

--- paper/code/Dare_LDA.R
+++ paper/code/Dare_LDA.R
@@ -17,7 +17,7 @@ Dare$edge = lapply(Dare$edge, function(x){x[1:3]})
 Daretest <- IPTM_inference.LDA(Dare$edge, Dare$node, Dare$text, Dare$vocab, nIP = 2, K = 2, sigma_Q = c(0.0000005, 0.1),
                         alpha = 2, mvec = rep(1/2, 2), betas = 2, nvec = rep(1/length(Dare$vocab), length(Dare$vocab)), 
                         prior.b.mean = c(-1, rep(0, 24)),
-                       prior.b.var = 1 * diag(25), prior.delta = c(0, 1), out = 100, n_B = 15000, n_d = 1500, burn = c(5000,500),
+                       prior.b.var = 1 * diag(25), prior.delta = c(0, 1), out = 3, n_B = 15000, n_d = 1500, burn = c(5000,500),
                        thinning = c(20,5), netstat = c("intercept", "dyadic", "degree", "triadic"), optimize = TRUE)
 Daretest_LDA = Daretest
 save(Daretest_LDA, file = "Daretest_LDA_K2.RData")
--- paper/code/HPC/Dare_full_nIP2.R
+++ paper/code/HPC/Dare_full_nIP2.R
@@ -13,6 +13,6 @@ Dare$edge = lapply(Dare$edge, function(x){x[1:3]})
 Daretest <- IPTM_inference.data(Dare$edge, Dare$node, Dare$text, Dare$vocab, nIP = 2, K = 20, sigma_Q = c(0.0000005, 0.01),
 alpha = 2, mvec = rep(1/20, 20), betas = 2, nvec = rep(1/length(Dare$vocab), length(Dare$vocab)),
                                 prior.b.mean = c(-1, rep(0, 24)),
-prior.b.var = diag(25), prior.delta = c(0, 1), out = 100, n_B = 15000, n_d = 1500, burn = c(5000, 500),
-thinning = c(20, 5),netstat = c("intercept", "dyadic", "degree", "triadic"), optimize = TRUE)
+prior.b.var = diag(25), prior.delta = c(0, 1), out = 100, n_B = 5500, n_d = 550, burn = c(500, 50),
+thinning = c(10, 1),netstat = c("intercept", "dyadic", "degree", "triadic"), optimize = TRUE)
 save(Daretest, file = "Daretest_IPTM_K20_nIP2.RData")
