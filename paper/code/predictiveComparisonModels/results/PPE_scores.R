87cf9204856b67fccb9eb91c2b3e61b450218c89
bomin8319 <bomin8319>
2017-09-25 15:16:56 -0400

    preliminary PPE plots and codes

--- paper/code/predictiveComparisonModels/results/PPE_scores.R
+++ paper/code/predictiveComparisonModels/results/PPE_scores.R
@@ -94,23 +94,23 @@ tables = tables[order(selectD[1:22])]
 all = Reduce('+', tables[-c(1,12)]) / length(tables[-c(1,12)])
 
 par(mfrow = c(1,3))
-plot(colnames(table_sum), table_sum[1,], type = 'l', col = 'red', xlab = "Number of Topics", lty = 2, ylab = "Sender AUC", ylim = c(table_sum[1,1] - 0.02, table_sum[1,1] + 0.02))
-lines(colnames(table_sum), all[1:7,1], lty = 3,col = "blue")
-lines(colnames(table_sum), all[8:14,1], lty = 4,col = "green")
-lines(colnames(table_sum), all[15:21,1], lty = 5,col = "purple")
-legend(60, 0.545, pch = 21, legend = c("IPTM with C = 3","IPTM with C = 2","IPTM with C = 1", "Regressions"), col = c("purple", "green", "blue", "red"))
+plot(colnames(table_sum), table_sum[1,], type = 'b', pch = 19, col = 'red', xlab = "Number of Topics", lty = 2, ylab = "Sender AUC", ylim = c(table_sum[1,1] - 0.02, table_sum[1,1] + 0.02))
+lines(colnames(table_sum), all[1:7,1], lty = 3,col = "blue", type = 'b', pch = 19)
+lines(colnames(table_sum), all[8:14,1], lty = 4,col = "green",type = 'b', pch = 19)
+lines(colnames(table_sum), all[15:21,1], lty = 5,col = "purple",type = 'b', pch = 19)
+legend(60, 0.545, pch = 19, bty = "n", lty = 2:5, legend = c("IPTM with C = 3","IPTM with C = 2","IPTM with C = 1", "Regressions"), col = c("purple", "green", "blue", "red"))
 
-plot(colnames(table_sum), table_sum[2,], type = 'l', col = 'red', xlab = "Number of Topics", lty = 2, ylab = "Receiver F1Score", ylim = c(table_sum[2,1] - 0.02, table_sum[2,1] + 0.01))
-lines(colnames(table_sum), all[1:7,2], lty = 3,col = "blue")
-lines(colnames(table_sum), all[8:14,2], lty = 4,col = "green")
-lines(colnames(table_sum), all[15:21,2], lty = 5,col = "purple")
-legend(60, 0.96, pch = 21, legend = c("IPTM with C = 3","IPTM with C = 2","IPTM with C = 1", "Regressions"), col = c("purple", "green", "blue", "red"))
+plot(colnames(table_sum), table_sum[2,], type = 'b', pch = 19,  col = 'red', xlab = "Number of Topics", lty = 2, ylab = "Receiver F1Score", ylim = c(table_sum[2,1] - 0.02, table_sum[2,1] + 0.01))
+lines(colnames(table_sum), all[1:7,2], lty = 3,col = "blue",type = 'b', pch = 19)
+lines(colnames(table_sum), all[8:14,2], lty = 4,col = "green",type = 'b', pch = 19)
+lines(colnames(table_sum), all[15:21,2], lty = 5,col = "purple",type = 'b', pch = 19)
+legend(60, 0.96,  pch = 19,bty = "n", lty = 2:5, legend = c("IPTM with C = 3","IPTM with C = 2","IPTM with C = 1", "Regressions"), col = c("purple", "green", "blue", "red"))
 
-plot(colnames(table_sum), table_sum[3,], type = 'l', col = 'red', xlab = "Number of Topics", lty = 2, ylab = "Time MSE", ylim = c(0, 1.6))
-lines(colnames(table_sum), all[1:7,3], lty = 3,col = "blue")
-lines(colnames(table_sum), all[8:14,3], lty = 4,col = "green")
-lines(colnames(table_sum), all[15:21,3], lty = 5,col = "purple")
-legend(60, 1.55, pch = 21, legend = c("IPTM with C = 3","IPTM with C = 2","IPTM with C = 1", "Regressions"), col = c("purple", "green", "blue", "red"))
+plot(colnames(table_sum), table_sum[3,], type = 'b', pch = 19, col = 'red', xlab = "Number of Topics", lty = 2, ylab = "Time MSE", ylim = c(0, 1.6))
+lines(colnames(table_sum), all[1:7,3], lty = 3,col = "blue",type = 'b', pch = 19)
+lines(colnames(table_sum), all[8:14,3], lty = 4,col = "green",type = 'b', pch = 19)
+lines(colnames(table_sum), all[15:21,3], lty = 5,col = "purple",type = 'b', pch = 19)
+legend(60, 1.55,  pch = 19, bty = "n",lty = 2:5, legend = c("IPTM with C = 3","IPTM with C = 2","IPTM with C = 1", "Regressions"), col = c("purple", "green", "blue", "red"))
 
 
 ##################################################
@@ -122,7 +122,7 @@ setwd('/Users/bomin8319/Desktop/IPTM/paper/code/predictiveComparisonModels/resul
  tables = matrix(0, nrow = 200, ncol = 3)
   colnames(tables) = c("sender", "receiver", "time")
   rownames(tables) = selectD
-for (d in 1:200) {
+for (d in 1:3) {
   D = selectD[d]
   observeds = tabulate(Dare$edge[[D]]$sender, 27)
   observedr = tabulate(Dare$edge[[D]]$receiver, 27)
@@ -142,8 +142,8 @@ for (d in 1:200) {
     tables[d, 3] = MSE / 100
   }
 
-tables = tables[order(selectD),]
-tables = tables[- which(tables[,3] > 50),]
+tables = tables[order(selectD[1:3]),]
+#tables = tables[- which(tables[,3] > 50),]
 
 par(mfrow = c(1,3))
 plot(rownames(tables), tables[,1], type = 'l', col = 'black', xlab = "Document", lty = 2, ylab = "Sender AUC", ylim = c(min(tables[,1]), max(tables[,1])), main = "Dare")
@@ -170,11 +170,11 @@ colnames(table_sum) = c("2", "5", "10", "25", "50", "75", "100")
 
 setwd('/Users/bomin8319/Desktop/IPTM/Dare_PPE')
 tables = list()
-for (d in 1:22) {
+for (d in 1:3) {
   D = selectD[d]
   observeds = tabulate(Dare$edge[[D]]$sender, 27)
   observedr = tabulate(Dare$edge[[D]]$receiver, 27)
-  observedt = Dare$edge[[D]]$timestamp
+  observedt = Dare$edge[[D]]$unixtime
   tables[[d]] = matrix(0, nrow = 21, ncol = 3)
   rownames(tables[[d]]) = 1:21
   iter = 1
@@ -191,7 +191,9 @@ for (d in 1:22) {
       for (r in 1:50) {
           if (r > 10 && (r-10) %% 4 == 0) {
         AUC = AUC + as.numeric(multiclass.roc(observeds, tabulate(PPE[[o]][[r]]$edge$sender, 27))$auc)
-        F1score = F1score + F1_Score(observedr, tabulate(PPE[[o]][[r]]$edge$receiver, 27))
+        F1 = F1_Score(observedr, tabulate(PPE[[o]][[r]]$edge$receiver, 27))
+        if (is.na(F1)) {F1 = 0}
+        F1score = F1score + F1
         MSE = MSE + (PPE[[o]][[r]]$edge$timestamp - observedt)^2
           it = it + 1
           }
@@ -205,9 +207,26 @@ for (d in 1:22) {
   }
 }
 
+tables = tables[order(selectD[1:3])]
+all = Reduce('+', tables) / length(tables)
+
 par(mfrow = c(1,3))
-plot(colnames(table_sum), table_sum[1,], type = 'l', col = 'red', xlab = "Document", lty = 2, ylab = "Sender AUC", main = "Enron")
-plot(colnames(table_sum), table_sum[2,], type = 'l', col = 'red', xlab = "Document", lty = 2, ylab = "Receiver F1Score", main = "Enron")
-plot(colnames(table_sum), table_sum[3,], type = 'l', col = 'red', xlab = "Document", lty = 2, ylab = "Time MSE", main = "Enron")
+plot(colnames(table_sum), table_sum[1,], type = 'b', pch = 19, col = 'red', xlab = "Number of Topics", lty = 2, ylab = "Sender AUC", ylim = c(table_sum[1,1] - 0.04, table_sum[1,1] + 0.02))
+lines(colnames(table_sum), all[1:7,1], lty = 3,col = "blue", type = 'b', pch = 19)
+lines(colnames(table_sum), all[8:14,1], lty = 4,col = "green",type = 'b', pch = 19)
+lines(colnames(table_sum), all[15:21,1], lty = 5,col = "purple",type = 'b', pch = 19)
+legend(60, 0.53, pch = 19, bty = "n", lty = 2:5, legend = c("IPTM with C = 3","IPTM with C = 2","IPTM with C = 1", "Regressions"), col = c("purple", "green", "blue", "red"))
+
+plot(colnames(table_sum), table_sum[2,], type = 'b', pch = 19,  col = 'red', xlab = "Number of Topics", lty = 2, ylab = "Receiver F1Score", ylim = c(table_sum[2,1] - 0.35, table_sum[2,1] + 0.15))
+lines(colnames(table_sum), all[1:7,2], lty = 3,col = "blue",type = 'b', pch = 19)
+lines(colnames(table_sum), all[8:14,2], lty = 4,col = "green",type = 'b', pch = 19)
+lines(colnames(table_sum), all[15:21,2], lty = 5,col = "purple",type = 'b', pch = 19)
+legend(60, 1.05,  pch = 19,bty = "n", lty = 2:5, legend = c("IPTM with C = 3","IPTM with C = 2","IPTM with C = 1", "Regressions"), col = c("purple", "green", "blue", "red"))
+
+plot(colnames(table_sum), table_sum[3,], type = 'b', pch = 19, col = 'red', xlab = "Number of Topics", lty = 2, ylab = "Time MSE", ylim = c(0, 0.1))
+lines(colnames(table_sum), all[1:7,3], lty = 3,col = "blue",type = 'b', pch = 19)
+lines(colnames(table_sum), all[8:14,3], lty = 4,col = "green",type = 'b', pch = 19)
+lines(colnames(table_sum), all[15:21,3], lty = 5,col = "purple",type = 'b', pch = 19)
+legend(60, 0.1,  pch = 19, bty = "n",lty = 2:5, legend = c("IPTM with C = 3","IPTM with C = 2","IPTM with C = 1", "Regressions"), col = c("purple", "green", "blue", "red"))
 
 
