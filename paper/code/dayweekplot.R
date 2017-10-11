92f58b8a9447021a108d33c26eb6ce733e599db6
bomin8319 <bomin8319>
2017-10-11 19:15:20 +0900

    updated box plot

--- paper/code/dayweekplot.R
+++ paper/code/dayweekplot.R
@@ -4,36 +4,23 @@ library(ergm)
 library(anytime)
 load('Darenew.RData')
 # 762 - 
-attach(Dare)
 Dare$edge = Dare$edge[-which(sapply(Dare$text, function(d){length(d)})==0)]
 Dare$text = Dare$text[-which(sapply(Dare$text, function(d){length(d)})==0)]
 
 wday = function(x) {
 	as.POSIXlt(x)$wday
 }
+timeinc = sapply(1:2209, function(x) Dare$edge[[x+1]]$unixtime - Dare$edge[[x]]$unixtime)
+weeks = sapply(1:2209, function(x) wday(anytime(Dare$edge[[x]]$unixtime,  tz = "America/New_York")) )
+day = sapply(1:2209, function(x) hour(ymd_hms(anytime(Dare$edge[[x]]$unixtime, tz = "America/New_York"))))
+day = cut(day, c(0,6,12,18,24), c("0-6", "6-12", "12-18", "18-24"))
 
-weeks = sapply(Dare$edge, function(x) wday(anytime(x$unixtime,  tz = "America/New_York")) )
-
-timeinc = c(sapply(1:2209, function(x) Dare$edge[[x+1]]$unixtime - Dare$edge[[x]]$unixtime), 0)
-boxplot(timeinc~weeks, names = c("Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"))
-
-rm(list=ls())
-setwd('/Users/bomin8319/Desktop/IPTM/full')
-library(fields)
-library(ergm)
-library(lubridate)
-load('Darenew.RData')
-# 762 - 
-attach(Dare)
-Dare$edge = Dare$edge[-which(sapply(Dare$text, function(d){length(d)})==0)]
-Dare$text = Dare$text[-which(sapply(Dare$text, function(d){length(d)})==0)]
-timeinc = c(sapply(1:2209, function(x) Dare$edge[[x+1]]$unixtime - Dare$edge[[x]]$unixtime), 0)
-
-day = sapply(Dare$edge, function(x) hour(ymd_hms(anytime(x$unixtime,  tz = "America/New_York"))))
-
-day = cut(day, c(0,6,9,14,Inf), c("Overnight", "Morning", "Afternoon", "Prime"))
-day[is.na(day)] = "Overnight" 
+weeks = weeks[-which(timeinc==0)]
+day = day[-which(timeinc==0)]
+timeinc = timeinc[-which(timeinc==0)]
 
 
-boxplot(timeinc~day)
+par(mfrow = c(1,2))
+boxplot(log(timeinc)~weeks, names = c("Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"))
+boxplot(log(timeinc)~day)
 
