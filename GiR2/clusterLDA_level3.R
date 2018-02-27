6c7389e8f8d67c177de2eb23175372498f7d28e8
bomin8319 <bomin8319>
2018-02-27 15:26:38 -0500

    slice sampling for alphas --> passed Schein test

--- GiR2/clusterLDA_level3.R
+++ GiR2/clusterLDA_level3.R
@@ -12,9 +12,17 @@ K = 4
 D = 10
 
 #samp is the number of GiR samples 
-samp = 1000     #takes about a minute with 1000
+samp = 5000     #takes about a minute with 1000
 results = matrix(NA, nrow = samp, ncol = 2*K)
 for (s in 1:samp) {
+# alpha = 5              #document-level parameter
+# alpha1 = 5			#cluster-level parameter
+# alpha0 = 5			#corpus-level parameter
+alpha = 10               #document-level parameter
+alpha1 = 10			#cluster-level parameter
+alpha0 = 10			#corpus-level parameter
+
+alphas = c(alpha0, alpha1, alpha)
 
 #assume IP is fixed
 nIP =2
@@ -55,6 +63,11 @@ table.k.null = tabulate(z, K)
 
 #inference on z using equation (15)---without word part that we ignored for simplified model
 for (i in 1:10){
+	alphas = slice(alphas, c(5,1,0.1), 5)
+	alpha0 = alphas[1]
+	alpha1 = alphas[2]
+	alpha = alphas[3]
+
   for (d in 1:D) {
     for (n in 1:n.d) {
       #excluding \dn
@@ -183,4 +196,40 @@ head(results)
 #currently it passes the test, but adjusting alphas (e.g. (10, 10, 10)) makes it fail
 
 
+slice = function(alphas, sigmas, S) {
+	for (s in 1:S) {
+		unew = runif(1, 0, f(alphas))
+		for (i in 1:length(alphas)) {
+			alphas.left[i] = max(0, alphas[i] - runif(1) * sigmas[i])
+			alphas.right[i] = max(0, alphas.left[i] + sigmas[i])
+		}	
+		while (TRUE){
+		alphas.new = sapply(1:length(alphas), function(i) runif(1, alphas.left[i], alphas.right[i]))
+		if (f(alphas.new) > unew) break
+		for (i in 1:length(alphas)) {
+			if (alphas.new[i] < alphas[i]) {
+				alphas.left[i] = max(0, alphas.new[i])
+			} else {
+				alphas.right[i] = max(0, alphas.new[i])
+			}
+		}
+		}
+		alphas = alphas.new
+	}
+	return(alphas)
+}
 
+
+f = function(alphas) {
+	alpha0 = alphas[1]
+	alpha1 = alphas[2]
+	alpha = alphas[3]
+	#prior = sum(dgamma(alphas, 1,1))
+	prob = 0
+	for (d in 1:D) {
+      ratio2 = (table.k+alpha0/K)/(sum(table.k)+alpha0)
+      ratio1 = (table.cd[,cd[d]]+alpha1*ratio2) / (sum(table.cd[,cd[d]])+alpha1)
+      prob = prob + sum(log((table.dk[,d]+alpha*ratio1) /(sum(table.dk[,d])+alpha)))
+  }
+	return(exp(prob))
+}
